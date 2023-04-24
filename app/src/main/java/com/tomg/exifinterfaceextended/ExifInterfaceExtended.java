94ae39a5e3c4f1e9644172b7475ccd36284d91b4
Ian Baker <ibaker@google.com>
2023-04-24 16:10:58 +0100

    Add missing space in ExifInterface exception message
    Bug: None
    Test: presubmit
    Change-Id: Ief499ae76627303803b9a07a027a7f3aea0f917a

--- app/src/main/java/com/tomg/exifinterfaceextended/ExifInterfaceExtended.java
+++ app/src/main/java/com/tomg/exifinterfaceextended/ExifInterfaceExtended.java
@@ -4348,7 +4348,7 @@ public class ExifInterfaceExtended {
             // Ignore exceptions in order to keep the compatibility with the old versions of
             // ExifInterface.
             if (DEBUG) {
-                Log.w(TAG, "Invalid image: ExifInterface got an unsupported image format file"
+                Log.w(TAG, "Invalid image: ExifInterface got an unsupported image format file "
                         + "(ExifInterface supports JPEG and some RAW image formats only) "
                         + "or a corrupted JPEG file to ExifInterface.", e);
             }
