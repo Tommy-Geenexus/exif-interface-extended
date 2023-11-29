f208a0bc6a4807be4bd2fdb178823805e5e1a74c
Sergey Vasilinetc <sergeyv@google.com>
2023-11-29 10:47:35 +0000

    Remove always false Build.VERSION.SDK_INT < (15, 16, 17, 18) if checks
    Test: refactoring
    Change-Id: I3f2e6d6d823481b5cfe577bb0d527839a9f46857

--- app/src/androidTest/java/com/tomg/exifinterfaceextended/ExifInterfaceExtendedTest.java
+++ app/src/androidTest/java/com/tomg/exifinterfaceextended/ExifInterfaceExtendedTest.java
@@ -1582,10 +1582,6 @@ public class ExifInterfaceExtendedTest {
      * <p>This does not check the image itself for similarity/equality.
      */
     private void assertBitmapsEquivalent(File expectedImageFile, File actualImageFile) {
-        if (Build.VERSION.SDK_INT < 16 && expectedImageFile.getName().endsWith("webp")) {
-            // BitmapFactory can't parse WebP files on API levels before 16: b/254571189
-            return;
-        }
         if (Build.VERSION.SDK_INT < 26
                 && expectedImageFile.getName().equals(WEBP_WITHOUT_EXIF_WITH_ANIM_DATA)) {
             // BitmapFactory can't parse animated WebP files on API levels before 26: b/259964971
