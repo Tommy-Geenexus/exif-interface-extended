xiaocainiaooo/room3

ExifInterface: support 0xFF padding
JPEG spec permits padding with 0xFF before any marker: "Any marker may
Any marker may optionally be preceded by any number of fill bytes,
which are bytes assigned code X’FF’."

Skip 0xFF padding when parsing JPEG metadata.

Bug: 459988448
Bug: 459698254
Test: ExifInterfaceTest
Change-Id: I5916b1fbaac760ab626a0ce97a3fc113ae9fa644

exifinterface/exifinterface/src/androidTest/java/androidx/exifinterface/media/ExpectedAttributes.java
@@ -114,6 +114,25 @@ final class ExpectedAttributes {
                     .setPixelYDimension(0)
                     .build();
 
+    /** Expected attributes for {@link R.raw#jpeg_with_fill_bytes}. */
+    public static final ExpectedAttributes JPEG_WITH_FILL_BYTES =
+            new Builder()
+                    .setThumbnailOffsetAndLength(3502, 6265)
+                    .setThumbnailSize(512, 288)
+                    .setIsThumbnailCompressed(true)
+                    .setMake("SAMSUNG")
+                    .setMakeOffset(162)
+                    .setModel("SM-N900S")
+                    .setAperture(2.2)
+                    .setDateTimeOriginal("2016:01:29 18:32:27")
+                    .setExposureTime(1.0 / 30)
+                    .setFocalLength("413/100")
+                    .setImageSize(640, 480)
+                    .setIso("50")
+                    .setPixelYDimension(480)
+                    .setOrientation(ExifInterface.ORIENTATION_ROTATE_90)
+                    .build();
+
     public static final ExpectedAttributes JPEG_WITH_APP1_AFTER_DQT =
             JPEG_WITH_EXIF_BYTE_ORDER_II
                     .buildUpon()

Tommy-Geenexus/exif-interface-extended

app/src/androidTest/java/io/github/tommygeenexus/exifinterfaceextended/ExpectedAttributes.java
@@ -116,6 +116,25 @@ final class ExpectedAttributes {
                     .setPixelYDimension(0)
                     .build();
 
+    /** Expected attributes for {@link R.raw#jpeg_with_fill_bytes}. */
+    public static final ExpectedAttributes JPEG_WITH_FILL_BYTES =
+            new Builder()
+                    .setThumbnailOffsetAndLength(3502, 6265)
+                    .setThumbnailSize(512, 288)
+                    .setIsThumbnailCompressed(true)
+                    .setMake("SAMSUNG")
+                    .setMakeOffset(162)
+                    .setModel("SM-N900S")
+                    .setAperture(2.2)
+                    .setDateTimeOriginal("2016:01:29 18:32:27")
+                    .setExposureTime(1.0 / 30)
+                    .setFocalLength("413/100")
+                    .setImageSize(640, 480)
+                    .setIso("50")
+                    .setPixelYDimension(480)
+                    .setOrientation(ExifInterfaceExtended.ORIENTATION_ROTATE_90)
+                    .build();
+
     public static final ExpectedAttributes JPEG_WITH_APP1_AFTER_DQT =
             JPEG_WITH_EXIF_BYTE_ORDER_II
                     .buildUpon()